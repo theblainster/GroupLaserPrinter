2b216adfe387b66dae4907e5a7d3372b4e8c5db5
theblainster <61989420+theblainster@users.noreply.github.com>
2020-11-13 08:40:23 -0600

    Add files via upload

---
+++ LaserPrinter.java
@@ -0,0 +1,59 @@
+public class LaserPrinter {
+	private boolean isOn = false;
+	private PaperAssembly paperTray;
+	
+	public LaserPrinter() {
+		paperTray = new PaperAssembly(300);
+	}
+	
+	// Body of logic
+
+	public void powerOn() {
+		if(isOn) {
+			System.out.println("Printer is already on.");
+		}
+		else {
+			System.out.println("Laser Printer - Starting up.");
+
+			try {
+				paperTray.activate();
+			} catch (Exception e) {
+				System.out.println("Exception was thrown while powering on the printer.  " + e);
+			}
+		
+			if(paperTray.isActive()) {
+				isOn = true;
+				System.out.println("Laser Printer successfully powered up.");
+			}
+		}
+	}
+	
+	public void reportStatus() {
+		if(isOn) {
+			System.out.println("--- Printer Status Report ---");
+			System.out.println("          Paper Level: " + paperTray.getValue());
+			System.out.println("--- Printer Status Report Complete ---");
+		} else {
+			System.out.println("Printer is off.");
+		}
+	}
+	
+	public void powerOff() {
+		if(isOn) {
+			System.out.println("Laser Printer - Shutting down.");
+		
+			try {
+				paperTray.deactivate();
+			} catch (Exception e) {
+				System.out.println("Exception was thrown while powering off the printer.  " + e);
+			}
+
+			if(!paperTray.isActive()) {
+				isOn = false;
+				System.out.println("Laser Printer successfully shutdown.");
+			}
+		} else {
+			System.out.println("Printer is already off.");
+		}
+	}
+}
